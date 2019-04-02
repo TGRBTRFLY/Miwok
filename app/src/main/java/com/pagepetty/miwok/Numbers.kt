8a1f8184ff743bd60f6ba99ecfdbb2acc3d3e237
PagePetty <pagevpetty@live.com>
2019-04-01 19:02:08 -0600

    add activities

---
+++ app/src/main/java/com/pagepetty/miwok/Numbers.kt
@@ -0,0 +1,12 @@
+package com.pagepetty.miwok
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Numbers : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_numbers)
+    }
+}
