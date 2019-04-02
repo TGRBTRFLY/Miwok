7936b738db4cfed46f1a8daacd360faac8948617
PagePetty <pagevpetty@live.com>
2019-04-01 22:06:44 -0600

    Modifying the Activity Name & Use an Intent to Open Another Activity

--- app/src/main/java/com/pagepetty/miwok/MainActivity.kt
+++ app/src/main/java/com/pagepetty/miwok/MainActivity.kt
@@ -5,11 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 
-
-
-
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +13,21 @@ class MainActivity : AppCompatActivity() {
 
     fun openNumbersList(view: View) {
         val i = Intent(this, Numbers::class.java)
+        startActivity(i)
+    }
+
+    fun openFamilyList(view: View) {
+        val i = Intent(this, FamilyMembers::class.java)
+        startActivity(i)
+    }
+
+    fun openColorsList(view: View) {
+        val i = Intent(this, Colors::class.java)
+        startActivity(i)
+    }
+
+    fun openPhrasesList(view: View) {
+        val i = Intent(this, Phrases::class.java)
         startActivity(i)
     }
 }
