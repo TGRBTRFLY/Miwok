39e900da5224b48307efe42793761a6507fcf621
PagePetty <pagevpetty@live.com>
2019-04-01 20:51:28 -0600

    onClick="openNumbersList"

--- app/src/main/java/com/pagepetty/miwok/MainActivity.kt
+++ app/src/main/java/com/pagepetty/miwok/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.pagepetty.miwok
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openNumbersList(view: View) {
+        val i = Intent(this, Numbers::class.java)
+        startActivity(i)
     }
 }
